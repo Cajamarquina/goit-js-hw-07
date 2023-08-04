0f257ac7f38045c085afced70b49fc5c3b4499ea
Cajamarquina <127974997+Cajamarquina@users.noreply.github.com>
2023-08-04 21:46:09 +0200

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,54 +3,27 @@ import { galleryItems } from './gallery-items.js';
 
 const galleryList = document.querySelector('.gallery');
 
-  // Function to create the gallery item template
-  function createGalleryItemTemplate(item) {
-    return `
-      <li class="gallery__item">
-        <a class="gallery__link" href="${item.original}">
-          <img
-            class="gallery__image"
-            src="${item.preview}"
-            data-source="${item.original}"
-            alt="${item.description}"
-          />
-        </a>
-      </li>
-    `;
-  }
-
-  // Render the gallery
-  function renderGallery() {
-    const galleryMarkup = galleryItems.map(createGalleryItemTemplate).join('');
-    galleryList.innerHTML = galleryMarkup;
-  }
-
-  // Open the modal with the full-size image
-  function openModal(event) {
-    event.preventDefault();
-    if (event.target.nodeName !== 'IMG') return;
-
+// Implement delegation to ul.gallery and handle click events
+galleryList.addEventListener('click', (event) => {
+  event.preventDefault();
+  if (event.target.tagName === 'IMG') {
     const largeImageUrl = event.target.dataset.source;
-    const instance = basicLightbox.create(`<img src="${largeImageUrl}">`);
-    instance.show();
-
-    // Close the modal on pressing the Escape key
-    function closeModalOnEsc(event) {
-    if (event.key === 'Escape') {
-    instance.close();
+    openModal(largeImageUrl);
   }
- }
-  
-    instance.show();
+});
 
-  // Enable keyboard listening while the modal window is open
-    document.addEventListener('keydown', closeModalOnEsc);
+// Open the modal window
+function openModal(imageUrl) {
+  const instance = basicLightbox.create(`<img src="${imageUrl}" alt="Full-size Image">`);
+  instance.show();
+
+   // Optional: Close the modal window on pressing the Escape key
+   function closeModalOnEscape(event) {
+    if (event.key === 'Escape') {
+      instance.close();
+      document.removeEventListener('keydown', closeModalOnEscape);
     }
+  }
 
-  // Attach event listener for the click event on the gallery list
-  galleryList.addEventListener('click', openModal);
-
-  // Render the initial gallery
-  renderGallery();
-
-console.log(galleryItems);
+  document.addEventListener('keydown', closeModalOnEscape);
+}
