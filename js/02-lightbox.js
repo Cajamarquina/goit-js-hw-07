237f205663e2492d0c9125dd4e2f672e3c878db9
Cajamarquina <127974997+Cajamarquina@users.noreply.github.com>
2023-08-04 21:55:01 +0200

    update

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,12 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryList = document.querySelector('.gallery');
+
+// Initialize SimpleLightbox after the gallery is created
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+  
 console.log(galleryItems);
