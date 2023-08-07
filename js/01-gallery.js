3edf247e646cf050a45826ba3212cf20d8d5af3f
Cajamarquina <127974997+Cajamarquina@users.noreply.github.com>
2023-08-07 14:10:34 +0200

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,6 +3,26 @@ import { galleryItems } from './gallery-items.js';
 
 const galleryList = document.querySelector('.gallery');
 
+// Render gallery items from galleryItems array
+function renderGallery() {
+  const galleryMarkup = galleryItems.map(item => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+        <img
+          class="gallery__image"
+          src="${item.preview}"
+          data-source="${item.original}"
+          alt="${item.description}"
+        />
+      </a>
+    </li>
+  `).join('');
+
+  galleryList.innerHTML = galleryMarkup;
+}
+
+renderGallery();
+
 // Implement delegation to ul.gallery and handle click events
 galleryList.addEventListener('click', (event) => {
   event.preventDefault();
@@ -12,10 +32,17 @@ galleryList.addEventListener('click', (event) => {
   }
 });
 
-// Open the modal window
 function openModal(imageUrl) {
-  const instance = basicLightbox.create(`<img src="${imageUrl}" alt="Full-size Image">`);
+  const instance = basicLightbox.create(`<img src="${imageUrl}" alt="Full-size Image">`, {
+    onShow: (instance) => {
+      document.addEventListener('keydown', closeModalOnEscape);
+    },
+    onClose: (instance) => {
+      document.removeEventListener('keydown', closeModalOnEscape);
+    },
+  });
   instance.show();
+
 
    // Optional: Close the modal window on pressing the Escape key
    function closeModalOnEscape(event) {
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,6 +3,23 @@ import { galleryItems } from './gallery-items.js';
 
 const galleryList = document.querySelector('.gallery');
 
+// Render gallery items from galleryItems array
+function renderGallery() {
+  const galleryMarkup = galleryItems.map(item => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+        <img
+          src="${item.preview}"
+          data-source="${item.original}"
+          alt="${item.description}"
+        />
+      </a>
+    </li>
+  `).join('');
+
+  galleryList.innerHTML = galleryMarkup;
+}
+
 // Initialize SimpleLightbox after the gallery is created
 const lightbox = new SimpleLightbox('.gallery a', {
     captionsData: 'alt',
